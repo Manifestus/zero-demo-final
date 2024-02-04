2f6ab23cd4b063090def13052e6344a36ac9d18e
Christopher Reay <christopherreay@gmail.com>
2024-02-04 07:22:06 -0600

    updated layout and structure of layout.tsx and auth/page.tsx

--- src/components/BottomMenu.tsx
+++ src/components/BottomMenu.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { useState } from "react";
+
 import Box  from "@mui/material/Box";
 import Link from "next/link";
 import LinkImageCentered from "./LinkImageCentered";
@@ -10,37 +12,44 @@ import PeopleAltOutlinedIcon from '@mui/icons-material/PeopleAltOutlined';
 import Diversity1OutlinedIcon from '@mui/icons-material/Diversity1Outlined';
 import TodayOutlinedIcon from '@mui/icons-material/TodayOutlined';
 
+import styles from "@/cssModules/BottomMenu.module.css";
+
 export default function BottomMenu() {
-  // const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(false);
+
+  const handleMenuMouseOver = () =>
+      {
+        setOpen(true);
+      }
+  const handleMenuMouseOut = () =>
+      {
+        setOpen(false);
+      }
 
  
   return (
     <Box sx={{  flexShrink: 0, py: 2, px: 3, textAlign: 'center', 
-                backgroundColor: 'rgba(255,255,255,0.6)',
-                borderRadius: "50% 50% 0px 0px",
-                position: "relative",
-                minHeight: "100px",
 
-    }} >
+    }} className={styles.curvedContainer} >
       <nav>
         <ul style={{listStyleType: "none"}}>
-          <li style={{ position: "absolute", top: "45px", left: "10%"}}>
+          <li style={{ position: "absolute", top: "40%", left: "10%"}}>
               <LinkImageCentered href="/page1"><MarkAsUnreadOutlinedIcon></MarkAsUnreadOutlinedIcon></LinkImageCentered>
           </li>
-          <li style={{ position: "absolute", top: "25px", left: "30%"}}>
+          <li style={{ position: "absolute", top: "23%", left: "30%"}}>
               <LinkImageCentered href="/page2"><PeopleAltOutlinedIcon></PeopleAltOutlinedIcon></LinkImageCentered>
           </li>
-          <li style={{ position: "absolute", top: "-15px", left: "50%"}}>
+          <li style={{ position: "absolute", top: "0px", left: "50%"}}>
               <LinkImageCentered href="/page3">
                 <div style={{height: "60px", width: "60px", borderRadius: "50%", backgroundColor: "rgba(255,255,255,0.7)"}}>
                   CA
                 </div>
               </LinkImageCentered>
           </li>
-          <li style={{ position: "absolute", top: "25px", left: "70%"}}>
+          <li style={{ position: "absolute", top: "23%", left: "70%"}}>
               <LinkImageCentered href="/page4"><Diversity1OutlinedIcon></Diversity1OutlinedIcon></LinkImageCentered>
           </li>
-          <li style={{ position: "absolute", top: "45px", left: "90%"}}>
+          <li style={{ position: "absolute", top: "40%", left: "90%"}}>
               <LinkImageCentered href="/page5"><TodayOutlinedIcon></TodayOutlinedIcon></LinkImageCentered>
           </li>
           
