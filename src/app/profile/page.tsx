985864972d2f3397d08598d79ae433438313f39c
Christopher Reay <christopherreay@gmail.com>
2024-02-04 16:45:13 -0600

    updated profile page structure #5

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -20,40 +20,19 @@ import SpaIcon from "@mui/icons-material/Spa";
 import GroupsIcon from "@mui/icons-material/Groups";
 import Diversity1Icon from "@mui/icons-material/Diversity1";
 
-export default function ProfilePage() {
-  return (
-    <Paper>
-      {/* <Box
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "flex-end",
-          height: "100vh",
-          background: "#f0f4f1",
-          position: "relative",
-        }}
-      > */}
-        <Box
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            // height: "75vh",
-            // width: "100%",
-            height: "100%",
-            width: "100%",
-            background:
-              "linear-gradient(0deg, rgba(191,194,254,1) 74%, rgba(232,189,219,1) 100%)",
-          }}
-          sx={{
-            bgcolor: "background.paper",
-            p: 3,
-            borderTopLeftRadius: 25,
-            borderTopRightRadius: 25,
-            boxShadow: 3,
-            position: "relative",
-          }}
-        >
-          <Avatar
+
+export default function ProfilePage(props: { children: React.ReactNode }) {
+  return <>
+      
+    <Box
+      sx={{display: "flex", flexDirection: "column",
+          alignItems: "center",
+          height: "calc(100% + 75px)", width:"100%",
+      }}
+    >
+    
+
+      <Avatar
             alt="Profile Picture"
             src="/static/images/avatar/1.jpg"
             sx={{
@@ -61,102 +40,94 @@ export default function ProfilePage() {
               height: "230px",
               // position: "absolute",
               // top: -110,
+              marginBottom: "-115px",
             }}
           />
-          <Grid
-            container
-            sx={{
-              // position: "absolute",
-              // top: 120,
-              justifyContent: "center",
-              alignItems: "center",
-              alignContent: "center",
-              justifyItems: "center",
-            }}
-            spacing={{ xs: 3 }}
-          >
-            <Grid
-              container
-              xs={4}
-              alignContent="center"
-              justifyContent="center"
-              justifyItems="center"
-              direction="column"
-            >
-              <Stack spacing={3}>
-                <Typography variant="h6">
-                  <SpaIcon />1
-                </Typography>
-                <Typography>
-                  <Diversity1Icon />2
-                </Typography>
-              </Stack>
-            </Grid>
-            <Grid container xs={5} justifyContent={"center"}>
-              <Stack spacing={1}>
-                <Typography
-                  variant="h1"
-                  component="h1"
-                  color={"black"}
-                  fontSize={"1rem"}
-                  fontWeight={"bold"}
-                  align="center"
-                >
-                  Princess Reema
-                </Typography>
-                <Typography
-                  variant="h1"
-                  component="h1"
-                  color={"black"}
-                  fontSize={"0.60rem"}
-                  fontWeight={"bold"}
-                  align="center"
-                >
-                  https://www.thevine.earth/princessreema
-                </Typography>
-              </Stack>
-            </Grid>
-            <Grid item xs={3}>
-              <Stack spacing={1} direction={"column"} alignItems={"center"}>
-                <Stack spacing={1}>
-                  <Facebook />
-                  <LinkedIn />
-                  <Instagram />
-                  <X />
-                </Stack>
-              </Stack>
-            </Grid>
-          </Grid>
-          <Box
-            sx={{
-              position: "absolute",
-              top: 260,
-              justifyContent: "center",
-              alignItems: "center",
-              alignContent: "center",
-              justifyItems: "center",
-            }}
-          >
-            <Stack sx={{ width: "80vw" }} spacing={2}>
-              <Divider sx={{ background: "white", borderBottomWidth: "2px" }} />
-              <Typography
-                variant="h1"
-                component="h1"
-                color={"black"}
-                fontSize={"0.70rem"}
-                align="center"
-                fontWeight={"bold"}
-              >
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat.
-              </Typography>
-              <Divider sx={{ background: "white", borderBottomWidth: "2px" }} />
-            </Stack>
-          </Box>
-        </Box>
-      {/* </Box> */}
-    </Paper>
-  );
+      
+      <Box
+        sx={{
+          flexGrow: 1, width:"100%", 
+          
+          borderRadius: "min(8vw, 48px) min(8vw, 48px) 0 0",
+
+          background: "linear-gradient(180deg, rgb(218 189 232) 0%, rgb(189 197 254) 50%, rgb(187 222 241) 100%)"
+        }}
+      >
+        {/* <StatsAndName></StatsAndName>
+        <Divider></Divider>
+        <Bio></Bio>
+        <Divider></Divider> */}
+
+      </Box>
+    </Box>
+    
+    </>
+}
+
+const StatsAndName =  () =>
+{
+  return <Box
+  sx={{
+    display: "flex", flexDirection: "row",
+
+    marginTop: "115px",
+
+    alignItems: "center"
+  }}
+>
+  <Box
+  sx={{
+    display: "flex", flexDirection: "column",
+    minWidth: "20%", maxWidth: "20%",
+  }}
+  >
+    <Box
+      sx={{
+        display: "flex", flexDirection: "row",
+      }}
+    >
+      <Box>TREE</Box>
+      <Box>1390</Box>
+    </Box>
+    <Box
+      sx={{
+        display: "flex", flexDirection: "row",
+      }}
+    >
+      <Box>Network</Box>
+      <Box>190</Box>
+    </Box>
+  </Box>
+  <Box
+    sx={{
+      display: "flex", flexDirection: "column",
+      flexGrow: 1,
+      flexShrink: 1,
+
+      alignItems: "center",
+    }}
+  >
+    <Box>Princess Reema</Box>
+    <Box>www.thevine.earth/princessreema</Box>
+
+  </Box>
+  <Box
+    sx={{
+      display: "flex", flexDirection: "column",
+      minWidth: "20%", maxWidth: "20%",
+      alignItems: "flex-end",
+    }}
+  >
+      <Box>LinkedIN</Box>
+      <Box>Whatever</Box>
+      <Box>Facebook</Box>
+      <Box>X</Box>
+  </Box>
+  
+</Box>
+}
+
+const Bio = () =>
+{
+  return <Box>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus facere illum ut totam qui, quae doloribus, neque laudantium, ab numquam animi deleniti eum? Eligendi suscipit quidem praesentium dicta quod quos.</Box>
 }
