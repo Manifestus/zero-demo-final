819719e27866bac7772a76f772b932e104b4d32c
Christopher Reay <christopherreay@gmail.com>
2024-02-04 12:29:57 -0600

    rebuilt the BottomMenu so it fits and looks right #1

--- src/components/BottomMenu.tsx
+++ src/components/BottomMenu.tsx
@@ -28,40 +28,55 @@ export default function BottomMenu() {
 
  
   return (
-    <Box sx={{  flexShrink: 0, py: 2, px: 3, textAlign: 'center', 
+    
+    
+    <Box className={styles.bottomMenu_container}>
 
-    }} className={styles.curvedContainer} >
-      <nav>
-        <ul style={{listStyleType: "none"}}>
-          <li style={{ position: "absolute", top: "40%", left: "10%"}}>
-              <LinkImageCentered href="/page1"><MarkAsUnreadOutlinedIcon></MarkAsUnreadOutlinedIcon></LinkImageCentered>
-          </li>
-          <li style={{ position: "absolute", top: "23%", left: "30%"}}>
-              <LinkImageCentered href="/page2"><PeopleAltOutlinedIcon></PeopleAltOutlinedIcon></LinkImageCentered>
-          </li>
-          <li style={{ position: "absolute", top: "0px", left: "50%"}}>
-              <LinkImageCentered href="/page3">
-                <div style={{height: "60px", width: "60px", borderRadius: "50%", backgroundColor: "rgba(255,255,255,0.7)"}}>
-                  CA
-                </div>
-              </LinkImageCentered>
-          </li>
-          <li style={{ position: "absolute", top: "23%", left: "70%"}}>
-              <LinkImageCentered href="/page4"><Diversity1OutlinedIcon></Diversity1OutlinedIcon></LinkImageCentered>
-          </li>
-          <li style={{ position: "absolute", top: "40%", left: "90%"}}>
-              <LinkImageCentered href="/page5"><TodayOutlinedIcon></TodayOutlinedIcon></LinkImageCentered>
-          </li>
-          
-          
-          {/* <li style={{ position: "absolute", top: "25px", left: "30%", }}><Link  href="/page1">Page 1</Link></li>
-          <li style={{ position: "absolute", top: "-15px", left: "50%", }}><Link  href="/page1">Page 1</Link></li>
-          <li style={{ position: "absolute", top: "25px", left: "70%", }}><Link  href="/page1">Page 1</Link></li>
-          <li style={{ position: "absolute", top: "50px", left: "90%", }}><Link  href="/page1">Page 1</Link></li> */}
-        </ul>
-      </nav>
+      
+
+      <Box className={styles.bottomMenu_curvedDiv_container}>
+        <Box sx={{  flexShrink: 0, py: 2, px: 3, textAlign: 'center', 
+          }} className={styles.curvedDiv}>
+        </Box>
+      </Box>
+
+      <Box className={styles.nav_container} >
+        <nav>
+          <ul style={{listStyleType: "none"}}>
+            <li style={{ position: "absolute", top: "78%", left: "10%"}}>
+                <LinkImageCentered href="/page1"><MarkAsUnreadOutlinedIcon></MarkAsUnreadOutlinedIcon></LinkImageCentered>
+            </li>
+            <li style={{ position: "absolute", top: "39%", left: "30%"}}>
+                <LinkImageCentered href="/page2"><PeopleAltOutlinedIcon></PeopleAltOutlinedIcon></LinkImageCentered>
+            </li>
+            <li style={{ position: "absolute", top: "0px", left: "50%"}}>
+                <LinkImageCentered href="/page3">
+                  <div style={{height: "60px", width: "60px", borderRadius: "50%", backgroundColor: "rgba(255,255,255,0.7)", textAlign:"center", paddingTop: "1em"}}>
+                    CA
+                  </div>
+                </LinkImageCentered>
+            </li>
+            <li style={{ position: "absolute", top: "39%", left: "70%"}}>
+                <LinkImageCentered href="/page4"><Diversity1OutlinedIcon></Diversity1OutlinedIcon></LinkImageCentered>
+            </li>
+            <li style={{ position: "absolute", top: "78%", left: "90%"}}>
+                <LinkImageCentered href="/page5"><TodayOutlinedIcon></TodayOutlinedIcon></LinkImageCentered>
+            </li>
+            
+            
+            {/* <li style={{ position: "absolute", top: "25px", left: "30%", }}><Link  href="/page1">Page 1</Link></li>
+            <li style={{ position: "absolute", top: "-15px", left: "50%", }}><Link  href="/page1">Page 1</Link></li>
+            <li style={{ position: "absolute", top: "25px", left: "70%", }}><Link  href="/page1">Page 1</Link></li>
+            <li style={{ position: "absolute", top: "50px", left: "90%", }}><Link  href="/page1">Page 1</Link></li> */}
+          </ul>
+        </nav>
+      </Box>
+
+      
     </Box>
-                    
+    
+    
+      
                     
   
   );
